c42bd576926509f338dcfa3993b9de950640d5e0
Josh <joshdevenyi@gmail.com>
2022-03-31 14:21:35 -0400

    Built Room CRUD, fixed Doctor Delete Issue

---
+++ HospitalApplication/Controllers/RoomDataController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HospitalApplication.Models;
+
+namespace HospitalApplication.Controllers
+{
+    public class RoomDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/RoomData/ListRooms
+        [HttpGet]
+        public IEnumerable<RoomDto> ListRooms()
+        {
+            List<Room> Rooms = db.Rooms.ToList();
+            List<RoomDto> RoomDtos = new List<RoomDto>();
+
+            Rooms.ForEach(r => RoomDtos.Add(new RoomDto()
+            {
+                RoomId = r.RoomId,
+                RoomPurpose = r.RoomPurpose,
+                RoomNumber = r.RoomNumber,
+                Floor = r.Floor,
+                Building = r.Building
+            }));
+
+            return RoomDtos;
+        }
+
+        // GET: api/RoomData/FindRoom/5
+        [ResponseType(typeof(Room))]
+        [HttpGet]
+        public IHttpActionResult FindRoom(int id)
+        {
+            Room room = db.Rooms.Find(id);
+            RoomDto RoomDto = new RoomDto()
+            {
+                RoomId = room.RoomId,
+                RoomPurpose = room.RoomPurpose,
+                RoomNumber = room.RoomNumber,
+                Floor = room.Floor,
+                Building = room.Building
+            };
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(RoomDto);
+        }
+
+        // PUT: api/RoomData/UpdateRoom/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateRoom(int id, Room room)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != room.RoomId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(room).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoomExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/RoomData/AddRoom
+        [ResponseType(typeof(Room))]
+        [HttpPost]
+        public IHttpActionResult AddRoom(Room room)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Rooms.Add(room);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = room.RoomId }, room);
+        }
+
+        // DELETE: api/RoomData/DeleteRoom/5
+        [ResponseType(typeof(Room))]
+        [HttpPost]
+        public IHttpActionResult DeleteRoom(int id)
+        {
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            db.Rooms.Remove(room);
+            db.SaveChanges();
+
+            return Ok(room);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RoomExists(int id)
+        {
+            return db.Rooms.Count(e => e.RoomId == id) > 0;
+        }
+    }
+}
